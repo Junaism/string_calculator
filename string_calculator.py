d6f79b8864fdcfea54d15b3f981ceb12d9b921a3
junais <mjunu2007@gmail.com>
2025-01-15 18:00:19 +0530

    feat: \n seperated srings are allowed

--- string_calculator.py
+++ string_calculator.py
@@ -11,9 +11,11 @@ class StringCalculator:
         """
         list_of_integers = [0]
         if value:
-           if re.findall('[^0-9,]', value): # check only contain integers with comma seperated
-               raise TypeError("Invalid input!, confirm input only contains integers that are comma seperated")
-           list_of_integers = value.split(",")
+            if re.findall(r'[^0-9,]', value.replace("\\n", ",")): # check only contain integers with comma and \n seperated
+               raise TypeError("Invalid input!, confirm input only contains integers that are comma or \\n seperated")
+            else:
+               value = value.replace('\\n', ',')
+               list_of_integers = value.split(",")
         else:
             return -1
         return sum([int(number.strip()) for number in list_of_integers if number.isdigit()])
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -15,8 +15,14 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(3, self.calculator.add("3,"))
         self.assertEqual(3, self.calculator.add(",3"))
         self.assertEqual(1+2+4+6+7+9+56, self.calculator.add("1,2,4,6,7,9,56"))
+        self.assertEqual(4+5, self.calculator.add("4,\\n5"))
+        self.assertEqual(3+9, self.calculator.add("3,9\\n"))
+        self.assertEqual(3+9, self.calculator.add("\\n3,9\\n"))
+        self.assertEqual(3+9+8, self.calculator.add("\\n3,9\\n8"))
     
     def test_error_are_handled_properly(self):
         """check validation errors are handled properly"""
         self.assertRaises(TypeError, self.calculator.add, "fr,56")
         self.assertRaises(TypeError, self.calculator.add, "f,9.0")
+        self.assertRaises(TypeError, self.calculator.add, "f,9.0\br")
+        self.assertRaises(TypeError, self.calculator.add, "f\b9\\")
